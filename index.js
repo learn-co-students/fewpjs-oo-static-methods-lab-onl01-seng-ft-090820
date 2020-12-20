4d5d9f8a325418f34a2d4b204a7a668272e71b00
Tesfaye Atomsa <tesfayeatomsa@gmail.com>
2020-12-20 16:45:59 -0700

    Done.

--- index.js
+++ index.js
@@ -6,17 +6,26 @@ class Formatter {
 
     static sanitize(string) {
         // removes any non alpha-numeric characters except dash and single quotes (apostrophes)
-        return string.replace(/[^A-Za-z0-9-']+/g, '');
+        return string.replace(/[^A-Za-z0-9- ']+/g, '');
     }
     static titleize(string) {
         let a = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
         let arr = string.split(" ");
-        for (let i = 0; i < arr.length; i++) {
-            let word = arr[i];
-            if (arr != a)
-                arr[i] = word[0].toUpperCase() + word.substring(1);
+
+        let newArr = [this.capitalize(arr[0])]
+        for (let i = 1; i < arr.length; i++) {
+            if (a.includes(arr[i])) {
+                newArr.push(arr[i])
+            } else {
+                newArr.push(this.capitalize(arr[i]))
+            }
+
+
+            // let word = arr[i];
+            // if (arr != a)
+            //     arr[i] = word[0].toUpperCase() + word.substring(1);
         }
 
-        return arr.join(" ");
+        return newArr.join(" ");
     };
 }
