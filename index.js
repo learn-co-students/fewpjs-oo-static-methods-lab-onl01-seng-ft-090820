c37b53a859642f0e82c64537dcca4ae2610d0976
doosemavis <moosedavis2011@gmail.com>
2020-12-31 14:17:24 -0600

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 class Formatter {
-  static capitalize() {
-
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  static sanitize() {
-
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]+/g, '');
   }
 
-  static titleize() {
-    
+  static titleize(string) {
+    let words = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let result = []
+    let array = string.split(' ')
+    for (let i = 0; i < array.length; i++) {
+      if (i === 0) {
+        result.push(this.capitalize(array[i]))
+      } else {
+          if (words.includes(array[i])) {
+            result.push(array[i])
+        } else {
+          result.push(this.capitalize(array[i]))
+        }
+      }
+    }
+    return result.join(' ')
   }
 }
